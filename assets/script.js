efaa4ac78bdd6dec113434df091acf2a57f44f2d
Zach Feldman <zach@nycda.com>
2016-08-17 09:31:14 -0400

    New version of the website, ready to go.

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,10 @@
 $ = jQuery
 $(document).ready(function(){
-  Cufon.replace('h1', {
-    hover: true,
-    hoverables: { h1: true }
-  });
-  $(".insta-slider .slide").first().show();
-  setInterval(function(){
-    if($(".insta-slider .slide:visible img").attr("src") == $(".insta-slider .slide img").last().attr("src")){
-      $(".insta-slider .slide:visible").fadeOut(function(){
-        $(".insta-slider .slide").first().fadeIn()
-      })
-    }else{
-      var visible_slide = $(".insta-slider .slide:visible")
-      visible_slide.fadeOut(function(){
-        visible_slide.next().fadeIn()
-      })
-    }
-  }, 5000)
-  $("h1").click(function(){
+  // Cufon.replace('h1', {
+  //   hover: true,
+  //   hoverables: { h1: true }
+  // });
+  $(".home h1").click(function(){
     $(".bio").slideToggle(1200);
   });
 });
